188b79adfb3f799910a34c5f796213d666bc3392
kuroakuro <hi.score.boy@gmail.com>
2018-04-13 15:26:40 +0900

    メイン更新

--- src/main/main.java
+++ src/main/main.java
@@ -27,6 +27,7 @@ public class main {
 
 
 		//敵があらわれた！
+		System.out.println("gitTest");
 		System.out.println( lastBoss.getName() + " が あらわれた！");
 		System.out.println( monster.getName() + " が あらわれた！");
 
